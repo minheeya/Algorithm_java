0bdabd1eb297b7c2f779c3e4f75357d5f526c666
minhee kang <himinhee1113@gmail.com>
2021-10-21 15:43:45 +0900

    TOPCODER 알고리즘 트레이닝 05) 전체탐색 : 암

---
+++ Algorithm/src/com/problems/algorithm/Cryptography.java
@@ -0,0 +1,20 @@
+package com.problems.algorithm;
+import java.util.*;
+
+public class Cryptography {
+	
+	public static long encrypt(int[] numbers) {
+		long mul = 1;
+		Arrays.sort(numbers);
+		numbers[0]++;
+		for(int i = 0; i < numbers.length; i++) {
+			mul *= numbers[i];
+		}
+		return mul;
+	}
+	
+	public static void main(String[] args) {
+		int[] numbers = {23, 5, 6, 25, 3, 8, 9, 10, 12};
+		System.out.println(encrypt(numbers));
+	}
+}
